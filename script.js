abc023cb05779f3244f73633747b7093c3ebe354
DePaul <CONF0577@depaul.edu>
2018-07-24 15:22:03 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready (function() }
+
+ <script>
+        var libButton = document.getElementById('lib-button');
+        var libIt = function() {
+            var storyDiv = document.getElementById("story");
+            storyDiv.innerHTML = "??Your story here";
+        };
+        libButton.addEventListener('click', libIt);
+        
+    </script>
+
+
+
+
+
+
+
+
+}};
